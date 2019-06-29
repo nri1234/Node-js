22e35f705f97a442cbcfd1ef6eac7cda92ba0cb3
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-06-29 17:31:05 +0200

    Change name of program.js>index.js init npm and install colors

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var ShapeTime = require('./Time');
 
 function getOSinfo() {
@@ -12,12 +13,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', ShapeTime.print(uptime));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.gray, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, Time.print(uptime));
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.grey, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
